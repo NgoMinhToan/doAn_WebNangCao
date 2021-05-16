dd8e8166a8972f8d1132c8e3413b3adfeca9dcd7
Ngo Minh Toan <mtoan2610@gmail.com>
2021-05-16 18:10:51 +0700

    1111

--- controllers/Notification.js
+++ controllers/Notification.js
@@ -11,7 +11,7 @@ module.exports = {
     index: (req, res) => {
         let success = req.flash('success')
         let msg = req.flash('msg')
-        return res.render('faculty', {data: {...req.session.passport.user, 
+        return res.render('facultyNotification', {data: {...req.session.passport.user, 
             ...req.user._doc}, flash: {success, msg}})
     },
     // flashData: (req, res, next) => {
